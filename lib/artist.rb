492028b52a845c1967b06742f421ed4eec3ee1dd
Eric Johnson <ericjohnson0216@icloud.com>
2018-04-07 17:04:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,6 +24,11 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.name
+    self.find(name) ? self.find(name) : self.create(name)
   end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
 end
